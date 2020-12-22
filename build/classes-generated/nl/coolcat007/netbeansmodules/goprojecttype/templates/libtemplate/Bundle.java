0c0c0667d3cf62f3c7ea833ff77897cfd9d2b715
FreddieMercy <junhao.zhang.freddie@gmail.com>
2020-12-22 01:54:10 -0800

    Really??? Added Dummy zip files then it works???

---
+++ build/classes-generated/nl/coolcat007/netbeansmodules/goprojecttype/templates/libtemplate/Bundle.java
@@ -0,0 +1,12 @@
+package nl.coolcat007.netbeansmodules.goprojecttype.templates.libtemplate;
+/** Localizable strings for {@link nl.coolcat007.netbeansmodules.goprojecttype.templates.libtemplate}. */
+class Bundle {
+    /**
+     * @return <i>Go Library</i>
+     * @see LibProjectTemplateWizardIterator
+     */
+    static String LibProjectTemplate_displayName() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LibProjectTemplate_displayName");
+    }
+    private Bundle() {}
+}
---
+++ build/classes-generated/nl/coolcat007/netbeansmodules/goprojecttype/templates/maintemplate/Bundle.java
@@ -0,0 +1,12 @@
+package nl.coolcat007.netbeansmodules.goprojecttype.templates.maintemplate;
+/** Localizable strings for {@link nl.coolcat007.netbeansmodules.goprojecttype.templates.maintemplate}. */
+class Bundle {
+    /**
+     * @return <i>Go Application</i>
+     * @see MainProjectTemplateWizardIterator
+     */
+    static String MainProjectTemplate_displayName() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "MainProjectTemplate_displayName");
+    }
+    private Bundle() {}
+}
